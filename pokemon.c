ddcfba48ba11ca1596bf75dce790796497c4cdbe
martinrein <martin.rein.toledano@gmail.com>
2020-09-13 06:16:47 +0800

    Fixed no damage and victory print bug

--- pokemon.c
+++ pokemon.c
@@ -15,7 +15,6 @@ void header(){
     printf("       \\_.-'       |__|    `-._  |              '-.|     '-.| |   |\n");
     printf("                               `'                             '-._|\n\n");
 }
-
 int
 selection(int nPoke, char Poke[]){
     switch(nPoke){
@@ -135,7 +134,7 @@ int ppskill(char move[], int pp){
         pp = 5;
     return pp;
 }
-int fight(float health, int bp, int move, int nPoke1, int nPoke2){
+int fight(float health, float bp, int move, int nPoke1, int nPoke2){
     int type1 = 0, type2 = 0;   // 1-fire, 2-water, 3-grass
     int stype1 = 0, stype2 = 0; // 4-electric, 5-ground, 6-ice
     float temp=1, temp2=1, temp3=1;
@@ -207,88 +206,102 @@ int fight(float health, int bp, int move, int nPoke1, int nPoke2){
             temp2 = 0.5;
         else if(stype1 == 3 || stype1 == 6)
             temp2 = 2;
-        else temp2 = 1;
+        else if(stype1 == 4)
+            temp2 = 1;
         
         if(stype2 == 1 || stype2 == 2 || stype2 == 5)
             temp3 = 0.5;
-        else if(stype1 == 3 || stype1 == 6)
+        else if(stype2 == 3 || stype2 == 6)
             temp3 = 2;
-        else temp3 = 1;
+        else if(stype2 == 4)
+            temp3 = 1;
     }
     else if(type1 == 2){
         if(stype1 == 2 || stype1 == 3)
             temp2 = 0.5;
         else if(stype1 == 1 || stype1 == 5)
             temp2 = 2;
-        else temp2 = 1;
+        else if(stype1 == 4 || stype1 == 6)
+            temp2 = 1;
         
         if(stype2 == 2 || stype2 == 3)
             temp3 = 0.5;
         else if(stype2 == 1 || stype2 == 5)
             temp3 = 2;
-        else temp3 = 1;
+        else if(stype2 == 4 || stype2 == 6)
+            temp3 = 1;
     }
     else if(type1 == 3){
         if(stype1 == 1 || stype1 == 3)
             temp2 = 0.5;
         else if(stype1 == 2 || stype1 == 5)
             temp2 = 2;
-        else temp2 = 0;
+        else if(stype1 == 4 || stype1 == 6)
+            temp2 = 0;
         
         if(stype2 == 1 || stype2 == 3)
             temp3 = 0.5;
         else if(stype2 == 2 || stype2 == 5)
             temp3 = 2;
-        else temp3 = 0;
+        else if(stype2 == 4 || stype2 == 6)
+            temp3 = 0;
     }
     else if(type1 == 4){
         if(stype1 == 1 || stype1 == 4 || stype1 == 6)
             temp2 = 1;
         else if(stype1 == 2)
             temp2 = 2;
-        else if (stype1 == 3)
+        else if(stype1 == 3)
             temp2 = 0.5;
-        else temp2 = 0;
+        else if(stype1 == 5)
+            temp2 = 0;
         
         if(stype2 == 1 || stype2 == 4 || stype2 == 6)
             temp3 = 1;
         else if(stype2 == 2)
             temp3 = 2;
-        else if (stype2 == 3)
+        else if(stype2 == 3)
             temp3 = 0.5;
-        else temp3 = 0;
+        else if(stype2 == 5)
+            temp3 = 0;
     }
     else if(type1 == 5){
         if(stype1 == 2 || stype1 == 3 || stype1 == 6)
             temp2 = 0.5;
         else if(stype1 == 1 || stype1 == 4)
             temp2 = 2;
-        else temp2 = 1;
+        else if(stype1 == 5)
+            temp2 = 1;
         
         if(stype2 == 2 || stype2 == 3 || stype2 == 6)
             temp3 = 0.5;
         else if(stype2 == 1 || stype2 == 4)
             temp3 = 2;
-        else temp3 = 1;
+        else if(stype2 == 5)
+            temp3 = 1;
     }
     else if(type1 == 6){
         if(stype1 == 1)
             temp2 = 0.5;
         else if(stype1 == 2 || stype1 == 3 || stype1 == 5)
             temp2 = 2;
-        else temp2 = 1;
+        else if(stype1 == 4 || stype1 == 6)
+            temp2 = 1;
         
         if(stype2 == 1)
             temp3 = 0.5;
         else if(stype2 == 2 || stype2 == 3 || stype2 == 5)
             temp3 = 2;
-        else temp3 = 1;
+        else if(stype2 == 4 || stype2 == 6)
+            temp3 = 1;
     }
+    printf("hp: %f, bp: %f, temp: %f, temp2: %f, temp3: %f", health, bp, temp, temp2, temp3);
     health = health - (bp * temp * temp2 * temp3);
     return health;
 }
 int main()
 {
+    pikachu();
     int game = 1;
     int choice = 0;
     int nPoke1, nPoke2;
@@ -296,8 +309,8 @@ int main()
     char Pokem1[15], Pokem2[15];
     char sPokem1[15], sPokem2[15];
     float hp1, hp2, shp1, shp2;       // health
-    int bp1, pp1, sbp1, spp1;       // 1st move
-    int bp2, pp2, sbp2, spp2;       // 2nd move
+    float bp1, pp1, sbp1, spp1;       // 1st move
+    float bp2, pp2, sbp2, spp2;       // 2nd move
     int pp3 = 5, spp3 = 5;          // 3rd move
     int pp4 = 5, spp4 = 5;          // 4th move
     int warning = 0, pro1 = 0, pro2 = 0, cbp = 0, scbp = 0;
@@ -347,141 +360,159 @@ int main()
         printf("%s: %.f/%.f HP\n\n", Pokemon2, shp1, shp2);
         if (game % 2 == 1){
             printf("%s's move are: \n", Pokemon1);
-            printf("  1 - %s (%d BP, %d PP)\n", Pokem1, bp1, pp1);
-            printf("  2 - %s (%d BP, %d PP)\n", Pokem2, bp2, pp2);
+            printf("  1 - %s (%.f BP, %.f PP)\n", Pokem1, bp1, pp1);
+            printf("  2 - %s (%.f BP, %.f PP)\n", Pokem2, bp2, pp2);
             printf("  3 - Protect (%d PP)\n", pp3);
             printf("  4 - Charge Up (%d PP)\n\n", pp4);
             game++;
-            do{
-                printf("Player 1, select a move: ");
-                scanf("%d", &choice);
-                warning = 0;
-                if (choice == 1){
-                    if(pp1 > 0){
-                        pp1--;
-                        pro1 = 0;
-                        if (cbp == 1){
-                            bp1 /=2;
-                            bp2 /=2;
+            if(pp1 != 0 || pp2 != 0 || pp3 != 0 || pp4 != 0){
+                do{
+                    printf("Player 1, select a move: ");
+                    scanf("%d", &choice);
+                    warning = 0;
+                    if (choice == 1){
+                        if(pp1 > 0){
+                            pp1--;
+                            if (pro2 == 0)
+                                shp1 = fight(shp1, bp1, choice, nPoke1, nPoke2);
+                            if (cbp == 1){
+                                bp1 /=2;
+                                bp2 /=2;
+                            }
+                            pro2 = 0;
                         }
-                        shp1 = fight(shp1, bp1, choice, nPoke1, nPoke2);
+                        else warning = 1;
                     }
-                    else warning = 1;
-                }
-                else if (choice == 2){
-                    if(pp2 > 0){
-                        pp2--;
-                        if (cbp == 1){
-                            bp1 /=2;
-                            bp2 /=2;
+                    else if (choice == 2){
+                        if(pp2 > 0){
+                            pp2--;
+                            if (pro2 == 0)
+                                shp1 = fight(shp1, bp2, choice, nPoke1, nPoke2);
+                            if (cbp == 1){
+                                bp1 /=2;
+                                bp2 /=2;
+                            }
+                            pro2 = 0;
                         }
-                        if (pro1 == 0)
-                            shp1 = fight(shp1, bp2, choice, nPoke1, nPoke2);
-                        pro1 = 0;
+                        else warning = 1;
                     }
-                    else warning = 1;
-                }
-                else if (choice == 3){
-                    if(pp3 > 0 && pro1 == 0){
-                        pp3--;
-                        pro1 = 1;
-                    }
-                    else warning = 1;
-                }
-                else if (choice == 4){
-                    if(pp4 > 0){
-                        pp4--;
-                        pro1 = 0;
-                        if (cbp == 0){
-                            bp1 *=2;
-                            bp2 *=2;
-                            cbp = 1;
+                    else if (choice == 3){
+                        if(pp3 > 0 && pro1 == 0){
+                            pp3--;
+                            pro1 = 1;
                         }
+                        else warning = 1;
                     }
-                    else warning = 1;
-                }
-                if(warning == 1 && pro1 == 1){
-                    printf("\nYou selected Protect last turn,\n");
-                    printf("you cannot Protect this turn\n\n");
-                }
-                else if(choice < 1 || choice > 4)
-                    printf("\nChoose a move number from 1 to 4!\n\n");
-                else if (warning == 1)
-                    printf("\nThat move has 0 PP.\n");
-            }while(warning == 1 || (choice < 1 || choice > 4));
+                    else if (choice == 4){
+                        if(pp4 > 0){
+                            pp4--;
+                            pro2 = 0;
+                            if (cbp == 0){
+                                bp1 *=2;
+                                bp2 *=2;
+                                cbp = 1;
+                            }
+                        }
+                        else warning = 1;
+                    }
+                    if(warning == 1 && pro1 == 1){
+                        printf("\nYou selected Protect last turn,\n");
+                        printf("you cannot Protect this turn\n\n");
+                    }
+                    else if(choice < 1 || choice > 4)
+                        printf("\nChoose a move number from 1 to 4!\n\n");
+                    else if (warning == 1)
+                        printf("\nThat move has 0 PP.\n");
+                }while(warning == 1 || (choice < 1 || choice > 4));
+            }
+            else{
+                printf("Sadly, you dont have enough PP,\n");
+                printf("you will be damaged by 5 HP and 10 HP for your opponent\n");
+                hp1 -= 5;
+                shp1 -= 10;
+            }
         }
         else if (game % 2 == 0){
             printf("%s's move are: \n", Pokemon2);
-            printf("  1 - %s (%d BP, %d PP)\n", sPokem1, sbp1, spp1);
-            printf("  2 - %s (%d BP, %d PP)\n", sPokem2, sbp2, spp2);
+            printf("  1 - %s (%.f BP, %.f PP)\n", sPokem1, sbp1, spp1);
+            printf("  2 - %s (%.f BP, %.f PP)\n", sPokem2, sbp2, spp2);
             printf("  3 - Protect (%d PP)\n", spp3);
             printf("  4 - Charge Up (%d PP)\n\n", spp4);
             game++;
-            do{
-                warning = 0;
-                printf("Player 2, select a move: ");
-                scanf("%d", &choice);
-                if (choice == 1){
-                    if(spp1 > 0){
-                        spp1--;
-                        pro2 = 0;
-                        if (scbp == 1){
-                            sbp1 /=2;
-                            sbp2 /=2;
+            if(spp1 != 0 || spp2 != 0 || spp3 != 0 || spp4 != 0){
+                do{
+                    warning = 0;
+                    printf("Player 2, select a move: ");
+                    scanf("%d", &choice);
+                    if (choice == 1){
+                        if(spp1 > 0){
+                            spp1--;
+                            if (pro1 == 0)
+                                hp1 = fight(hp1, sbp1, choice, nPoke2, nPoke1);
+                            if (scbp == 1){
+                                sbp1 /=2;
+                                sbp2 /=2;
+                            }
+                            pro1 = 0;
                         }
-                        hp1 = fight(hp1, sbp1, choice, nPoke2, nPoke1);
+                        else warning = 1;
                     }
-                    else warning = 1;
-                }
-                else if (choice == 2){
-                    if(spp2 > 0){
-                        spp2--;
-                        if (scbp == 1){
-                            sbp1 /=2;
-                            sbp2 /=2;
+                    else if (choice == 2){
+                        if(spp2 > 0){
+                            spp2--;
+                            if (pro1 == 0)
+                                hp1 = fight(hp1, sbp2, choice, nPoke2, nPoke1);
+                            if (scbp == 1){
+                                sbp1 /=2;
+                                sbp2 /=2;
+                            }
+                            pro1 = 0;
                         }
-                        if (pro2 == 0)
-                            hp1 = fight(hp1, sbp2, choice, nPoke2, nPoke1);
-                        pro2 = 0;
+                        else warning = 1;
                     }
-                    else warning = 1;
-                }
-                else if (choice == 3){
-                    if(spp3 > 0 && pro2 == 0){
-                        spp3--;
-                        pro2 = 1;
-                    }
-                    else warning = 1;
-                }
-                else if (choice == 4){
-                    if(spp4 > 0){
-                        spp4--;
-                        pro2 = 0;
-                        if (scbp == 0){
-                            sbp1 *=2;
-                            sbp2 *=2;
-                            scbp = 1;
+                    else if (choice == 3){
+                        if(spp3 > 0 && pro2 == 0){
+                            spp3--;
+                            pro2 = 1;
                         }
+                        else warning = 1;
                     }
-                    else warning = 1;
-                }
-                if(warning == 1 && pro2 == 1){
-                    printf("\nYou selected Protect last turn,\n");
-                    printf("you cannot Protect this turn\n\n");
-                }
-                else if(choice < 1 || choice > 4)
-                    printf("\nChoose a move number from 1 to 4!\n\n");
-            }while(warning == 1 || (choice < 1 || choice > 4));
+                    else if (choice == 4){
+                        if(spp4 > 0){
+                            spp4--;
+                            pro1 = 0;
+                            if (scbp == 0){
+                                sbp1 *=2;
+                                sbp2 *=2;
+                                scbp = 1;
+                            }
+                        }
+                        else warning = 1;
+                    }
+                    if((warning == 1 && pro2 == 1) || (warning == 1 && pro1 == 1)){
+                        printf("\nYou selected Protect last turn,\n");
+                        printf("you cannot Protect this turn\n\n");
+                    }
+                    else if(choice < 1 || choice > 4)
+                        printf("\nChoose a move number from 1 to 4!\n\n");
+                }while(warning == 1 || (choice < 1 || choice > 4));
+            }
+            else{
+                printf("Sadly, you dont have enough PP,\n");
+                printf("you will be damaged by 5 HP and 10 HP for your opponent\n");
+                shp1 -= 5;
+                hp1 -= 10;
+            }
         }
         printf("\n**************************************************\n\n");
     }while (hp1 > 0 && shp1 > 0);
     
-    if(hp1 < 0){
+    if(hp1 <= 0){
         printf("The Pokemon of Player 1 has 0 HP!\n");
         printf("Player 2 wins!\n\n");
     }
-    else if(hp2 < 0){
-        printf("The Pokemon of Player 1 has 0 HP!\n");
+    else if(shp1 <= 0){
+        printf("The Pokemon of Player 2 has 0 HP!\n");
         printf("Player 1 wins!\n\n");
     }
     printf("**************************************************\n\n");
